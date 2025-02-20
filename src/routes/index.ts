99f9f1c0b75ea80db50e484e27405f8e3f8b477d
Niraj palmur <niraj.palmur@emergys.com>
2025-02-20 11:56:01 +0530

    added subform route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import productRoutes from "./product.routes";
 import gatesRoutes from "./gates.routes";
 import userRoutes from "./user.routes";
 import formRoutes from "./form.routes";
+import subformRoutes from "./subForms.routes"
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use("/products", productRoutes);
 router.use("/gates", gatesRoutes);
 router.use("/user", userRoutes);
 router.use("/forms",formRoutes);
+router.use("/subforms",formRoutes);
 
 export default router;
